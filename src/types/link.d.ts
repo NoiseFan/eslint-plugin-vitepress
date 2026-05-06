f7c053b34fbd06ecb548627fd0a540b9426d7c6b
noise <noisefan@163.com>
2026-05-06 15:00:00 +0800

    typo: remove deprecated file

--- src/types/link.d.ts
+++
@@ -1,6 +0,0 @@
-import type { LINK_SPACE_MESSAGE_IDS } from '../utils/rules/link'
-
-/**
- * The allowed issue ids for link spacing rules.
- */
-export type LinkSpaceIssue = typeof LINK_SPACE_MESSAGE_IDS[keyof typeof LINK_SPACE_MESSAGE_IDS]
